204c0c535bbe9a9824e5b653ebda6c67a7c1f6a0
Samir Murati <sam@vis-ag.ch>
2023-03-02 08:52:57 +0000

    footer controller

--- salesorders/webapp/controller/Worklist.controller.js
+++ salesorders/webapp/controller/Worklist.controller.js
@@ -3,8 +3,10 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/FilterType",
+], function (BaseController, JSONModel, formatter, Filter, FilterOperator, Sorter, FilterType) {
     "use strict";
 
     return BaseController.extend("sapb1.salesorders.controller.Worklist", {
@@ -19,22 +21,32 @@ sap.ui.define([
          * Called when the worklist controller is instantiated.
          * @public
          */
-        onInit : function () {
+        onInit: function () {
             var oViewModel;
-
+            var oMessageModelBinding;
+            var oMessageManager = sap.ui.getCore().getMessageManager(),
+				oMessageModel = oMessageManager.getMessageModel(),
+				oMessageModelBinding = oMessageModel.bindList("/", undefined, [],
+					new Filter("technical", FilterOperator.EQ, true))
             // keeps the search state
             this._aTableSearchState = [];
 
             // Model used to manipulate control states
             oViewModel = new JSONModel({
-                worklistTableTitle : this.getResourceBundle().getText("worklistTableTitle"),
+                worklistTableTitle: this.getResourceBundle().getText("worklistTableTitle"),
                 shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
                 shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
-                tableNoDataText : this.getResourceBundle().getText("tableNoDataText")
+                tableNoDataText: this.getResourceBundle().getText("tableNoDataText"),
+                busy : false,
+				order : 0,
+                hasUIChanges : false,
+				cardnameEmpty : true,
             });
             this.setModel(oViewModel, "worklistView");
-            var data = this.getView()
-            console.log("DATA", data);
+            this.getView().setModel(oMessageModel, "message");
+            
+            oMessageModelBinding.attachChange(this.onMessageBindingChange, this);
+			this._bTechnicalErrors = false;
         },
 
         /* =========================================================== */
@@ -50,7 +62,41 @@ sap.ui.define([
          * @param {sap.ui.base.Event} oEvent the update finished event
          * @public
          */
-        onUpdateFinished : function (oEvent) {
+
+        onCreate : function () {
+			var oList = this.byId("table"),
+				oBinding = oList.getBinding("items"),
+				oContext = oBinding.create({
+                    "DocNum": "",
+					"CardName" : "",
+					"Comments" : "",
+					"DocTotal" : "",
+				});
+
+			this._setUIChanges();
+			this.getView().getModel("worklistView").setProperty("/cardnameEmpty", true);
+
+			oList.getItems().some(function (oItem) {
+				if (oItem.getBindingContext() === oContext) {
+					oItem.focus();
+					oItem.setSelected(true);
+					return true;
+				}
+			});
+		},
+
+        onInputChange : function (oEvt) {
+			if (oEvt.getParameter("escPressed")) {
+				this._setUIChanges();
+			} else {
+				this._setUIChanges(true);
+				if (oEvt.getSource().getParent().getBindingContext().getProperty("CardName")) {
+					this.getView().getModel("worklistView").setProperty("/cardnameEmpty", false);
+				}
+			}
+		},
+
+        onUpdateFinished: function (oEvent) {
             // update the worklist's object counter after the table update
             var sTitle,
                 oTable = oEvent.getSource(),
@@ -70,7 +116,7 @@ sap.ui.define([
          * @param {sap.ui.base.Event} oEvent the table selectionChange event
          * @public
          */
-        onPress : function (oEvent) {
+        onPress: function (oEvent) {
             // The source is the list item that got pressed
             this._showObject(oEvent.getSource());
         },
@@ -80,13 +126,13 @@ sap.ui.define([
          * Navigate back in the browser history
          * @public
          */
-        onNavBack : function() {
+        onNavBack: function () {
             // eslint-disable-next-line sap-no-history-manipulation
             history.go(-1);
         },
 
 
-        onSearch : function (oEvent) {
+        onSearch: function (oEvent) {
             console.log("search is clicked");
             if (oEvent.getParameters().refreshButtonPressed) {
                 // Search field's 'refresh' button has been pressed.
@@ -111,10 +157,34 @@ sap.ui.define([
          * and group settings and refreshes the list binding.
          * @public
          */
-        onRefresh : function () {
+        onRefresh: function () {
             var oTable = this.byId("table");
             oTable.getBinding("items").refresh();
         },
+
+        onResetChanges : function () {
+			this.byId("table").getBinding("items").resetChanges();
+			this._bTechnicalErrors = false; 
+			this._setUIChanges();
+		},
+
+        onSave : function () {
+			var fnSuccess = function () {
+				this._setBusy(false);
+				MessageToast.show(this._getText("changesSentMessage"));
+				this._setUIChanges(false);
+			}.bind(this);
+
+			var fnError = function (oError) {
+				this._setBusy(false);
+				this._setUIChanges(false);
+				MessageBox.error(oError.message);
+			}.bind(this);
+
+			this._setBusy(true); // Lock UI until submitBatch is resolved.
+			this.getView().getModel().submitBatch("orderGoup").then(fnSuccess, fnError);
+			this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
+		},
 
         /* =========================================================== */
         /* internal methods                                            */
@@ -125,7 +195,7 @@ sap.ui.define([
          * @param {sap.m.ObjectListItem} oItem selected Item
          * @private
          */
-        _showObject : function (oItem) {
+        _showObject: function (oItem) {
             this.getRouter().navTo("object", {
                 objectId: oItem.getBindingContext().getPath().substring("/Orders".length)
             });
@@ -136,7 +206,7 @@ sap.ui.define([
          * @param {sap.ui.model.Filter[]} aTableSearchState An array of filters for the search
          * @private
          */
-        _applySearch: function(aTableSearchState) {
+        _applySearch: function (aTableSearchState) {
             var oTable = this.byId("table"),
                 oViewModel = this.getModel("worklistView");
             oTable.getBinding("items").filter(aTableSearchState, "Application");
@@ -145,6 +215,84 @@ sap.ui.define([
                 oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
             }
         }
+        ,
+        // ---------------------------------------------------------------------------
+        onRefresh: function () {
+            var oBinding = this.byId("table").getBinding("items");
 
+            if (oBinding.hasPendingChanges()) {
+                sap.m.MessageBox.error(this._getText("refreshNotPossibleMessage"));
+                return;
+            }
+            oBinding.refresh();
+            sap.m.MessageBox.show(this._getText("refreshSuccessMessage"))
+        },
+
+        _getText : function (sTextId, aArgs) {
+        	return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
+
+        },
+
+        onSort : function () {
+			var oView = this.getView(),
+				aStates = [undefined, "asc", "desc"],
+				aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+				sMessage,
+				iOrder = oView.getModel("worklistView").getProperty("/order");
+
+			iOrder = (iOrder + 1) % aStates.length;
+			var sOrder = aStates[iOrder];
+
+			oView.getModel("worklistView").setProperty("/order", iOrder);
+			oView.byId("table").getBinding("items").sort(sOrder && new Sorter("CardName", sOrder === "desc"));
+
+			sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+			sap.m.MessageToast.show(sMessage);
+			},
+
+        // ------------------- Create edit data
+
+        onMessageBindingChange : function (oEvent) {
+			var aContexts = oEvent.getSource().getContexts(),
+				aMessages,
+				bMessageOpen = false;
+
+			if (bMessageOpen || !aContexts.length) {
+				return;
+			}
+
+			// Extract and remove the technical messages
+			aMessages = aContexts.map(function (oContext) {
+				return oContext.getObject();
+			});
+			sap.ui.getCore().getMessageManager().removeMessages(aMessages);
+
+			this._setUIChanges(true);
+			this._bTechnicalErrors = true;
+			MessageBox.error(aMessages[0].message, {
+				id : "serviceErrorMessageBox",
+				onClose : function () {
+					bMessageOpen = false;
+				}
+			});
+
+			bMessageOpen = true;
+		},
+
+		_setUIChanges : function (bHasUIChanges) {
+			if (this._bTechnicalErrors) {
+				// If there is currently a technical error, then force 'true'.
+				bHasUIChanges = true;
+			} else if (bHasUIChanges === undefined) {
+				bHasUIChanges = this.getView().getModel().hasPendingChanges();
+			}
+			var oModel = this.getView().getModel("worklistView");
+			oModel.setProperty("/hasUIChanges", bHasUIChanges);
+		},
+        
+		_setBusy : function (bIsBusy) {
+			var oModel = this.getView().getModel("worklistView");
+			oModel.setProperty("/busy", bIsBusy);
+		}
     });
 });
