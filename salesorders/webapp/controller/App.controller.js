59081be30ed66f979b848662291f33c631f23543
Samir Murati <sam@vis-ag.ch>
2023-03-03 15:04:37 +0000

    side navbar

--- salesorders/webapp/controller/App.controller.js
+++ salesorders/webapp/controller/App.controller.js
@@ -1,11 +1,31 @@
 sap.ui.define([
-    "./BaseController"
-], function (BaseController) {
+    "./BaseController",
+    "sap/ui/core/ComponentContainer",
+   "sap/ui/core/UIComponent"
+], function (BaseController, ComponentContainer, UIComponent) {
     "use strict";
 
     return BaseController.extend("sapb1.salesorders.controller.App", {
 
         onInit : function () {
+            // var sideNavComponent = new ComponentContainer({
+            //     name: "sapb1.salesorders",
+            //     settings: {
+            //        id: "SideNav"
+            //     }
+            //  });
+            //  var worklistComponent = new ComponentContainer({
+            //     name: "sapb1.salesorders",
+            //     settings: {
+            //        id: "worklist"
+            //     }
+            //  });
+    
+            //  var oSideNavSplitContainer = this.byId("sideNavSplitContainer");
+            //  oSideNavSplitContainer.addContent(sideNavComponent);
+    
+            //  var oWorklistSplitContainer = this.byId("worklistSplitContainer");
+            //  oWorklistSplitContainer.addContent(worklistComponent);
             // apply content density mode to root view
             this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
         }
--- salesorders/webapp/controller/Split.controller.js
+++ salesorders/webapp/controller/Split.controller.js
@@ -1,14 +1,68 @@
 sap.ui.define([
-    "./BaseController"
-], function (BaseController) {
-    "use strict";
+	"sap/m/MessageToast",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/Device",
+	"sap/base/Log"
+], function (MessageToast, Controller, Device, Log) {
+	"use strict";
 
-    return BaseController.extend("sapb1.salesorders.controller.Split", {
+	return Controller.extend("sapb1.salesorders.controller.Split", {
 
-        // onInit : function () {
-        //     // apply content density mode to root view
-        //     this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-        // }
-    });
+		onInit: function () {
+			this.getSplitAppObj().setHomeIcon({
+				'phone': 'phone-icon.png',
+				'tablet': 'tablet-icon.png',
+				'icon': 'desktop.ico'
+			});
 
+			Device.orientation.attachHandler(this.onOrientationChange, this);
+		},
+
+		onExit: function () {
+			Device.orientation.detachHandler(this.onOrientationChange, this);
+		},
+
+		onOrientationChange: function (mParams) {
+			var sMsg = "Orientation now is: " + (mParams.landscape ? "Landscape" : "Portrait");
+			MessageToast.show(sMsg, { duration: 5000 });
+		},
+
+		onPressNavToDetail: function () {
+			this.getSplitAppObj().to(this.createId("detailDetail"));
+		},
+
+		onPressDetailBack: function () {
+			this.getSplitAppObj().backDetail();
+		},
+
+		onPressMasterBack: function () {
+			this.getSplitAppObj().backMaster();
+		},
+
+		onPressGoToMaster: function () {
+			this.getSplitAppObj().toMaster(this.createId("master2"));
+		},
+
+		onListItemPress: function (oEvent) {
+			var sToPageId = oEvent.getParameter("listItem").getCustomData()[0].getValue();
+
+			this.getSplitAppObj().toDetail(this.createId(sToPageId));
+		},
+
+		onPressModeBtn: function (oEvent) {
+			var sSplitAppMode = oEvent.getSource().getSelectedButton().getCustomData()[0].getValue();
+
+			this.getSplitAppObj().setMode(sSplitAppMode);
+			MessageToast.show("Split Container mode is changed to: " + sSplitAppMode, { duration: 5000 });
+		},
+
+		getSplitAppObj: function () {
+			var result = this.byId("SplitAppDemo");
+			if (!result) {
+				Log.info("SplitApp object can't be found");
+			}
+			return result;
+		}
+
+	});
 });
